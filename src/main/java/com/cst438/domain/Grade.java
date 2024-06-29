91f759687adce0998da597d2f5eb61afb011064a
harisj739 <hjilani@csumb.edu>
2024-06-29 15:40:39 -0700

    Updated the Assignment, Enrollment, and Grade entity classes and repository interfaces.

--- src/main/java/com/cst438/domain/Grade.java
+++ src/main/java/com/cst438/domain/Grade.java
@@ -8,10 +8,18 @@ public class Grade {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="grade_id")
     private int gradeId;
- 
-    // TODO complete this class
+
     // add additional attribute for score
     // add relationship between grade and assignment entities
     // add relationship between grade and enrollment entities
     // add getter/setter methods
+    private Integer score;
+    @ManyToOne
+    @JoinColumn(name="assignment_id", nullable=false)
+    private Assignment assignment;
+    @ManyToOne
+    @JoinColumn(name="enrollment_id", nullable=false)
+    private Enrollment enrollment;
+ 
+    // TODO complete this class
 }
