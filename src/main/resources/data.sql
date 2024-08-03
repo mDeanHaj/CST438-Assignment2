637bf7ec5f54e86af6228ce3bb51137cb5fef7fd
Superdean56 <mhajjar@csumb.edu>
2024-08-03 16:01:48 -0700

    Imported the Zip files for REST

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,15 +3,16 @@ insert into term (term_id, tyear, semester, add_date, add_deadline, drop_deadlin
 (6, 2022, 'Fall',   '2022-05-01', '2022-08-30', '2022-08-30', '2022-08-15', '2022-12-17'),
 (7, 2023, 'Spring', '2022-11-01', '2023-01-30', '2023-01-30', '2023-01-15', '2023-05-17'),
 (8, 2023, 'Fall' ,  '2023-05-01', '2023-08-30', '2023-08-30', '2023-08-15', '2023-12-17'),
-(9, 2024, 'Spring', '2023-11-01', '2024-04-30', '2024-04-30', '2024-01-15', '2024-05-17'),
-(10, 2024, 'Fall',  '2024-05-01', '2024-08-30', '2024-08-30', '2024-08-15', '2024-12-17'),
-(11, 2025, 'Spring', '2024-07-13', '2024-09-05', '2024-10-05', '2024-12-20', '2025-01-15');
+(9, 2024, 'Spring', '2023-11-01', '2024-03-30', '2024-03-30', '2024-01-15', '2024-05-17'),
+(10, 2024, 'Fall',  '2024-05-01', '2024-08-30', '2024-08-30', '2024-08-15', '2024-12-17');
 
 insert into user_table (id, name, email, password, type) values
 (1, 'david wisneski', 'dwisneski@csumb.edu', '', 'INSTRUCTOR'),
 (2, 'joshua gross', 'jgross@csumb.edu', '', 'INSTRUCTOR'),
 (3, 'thomas edison', 'tedison@csumb.edu', '', 'STUDENT'),
-(4, 'fred brooks', 'fbrooks@csumb.edu', '', 'ADMIN');
+(4, 'fred brooks', 'fbrooks@csumb.edu', '', 'ADMIN'),
+(5, 'user', 'user@csumb.edu', '$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue','STUDENT'),
+(6, 'admin', 'admin@csumb.edu', '$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW' , 'ADMIN');
 
 insert into course values
 ('cst238', 'Introduction to Programming', 5),
@@ -34,9 +35,7 @@ insert into section(section_no, course_id, sec_id, term_id, building, room, time
 (7, 'cst338', 2, 9, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
 (8, 'cst363', 1, 9, '052', '104', 'M W 10:00-11:50', 'dwisneski@csumb.edu'),
 (9, 'cst363', 2, 9, '052', '102', 'M W 2:00-3:50', 'dwisneski@csumb.edu'),
-(10, 'cst438', 1, 9, '052', '222', 'T Th 12:00-1:50', 'dwisneski@csumb.edu'),
-(11, 'cst338', 1, 10, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
-(12, 'cst363', 1, 11, '052', '104', 'M W 10:00-11:50', 'dwisneski@csumb.edu');
+(10, 'cst438', 1, 9, '052', '222', 'T Th 12:00-1:50', 'dwisneski@csumb.edu');
 
 insert into enrollment (enrollment_id, grade, section_no, user_id ) values
 (1, 'A', 1, 3),
