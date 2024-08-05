fdd48a604c2644eef6e68de2c2f251c9a55f5ae5
Mohe Dean Hajjar <mhajjar@csumb.edu>
2024-08-05 16:04:22 -0700

    Completed:
    - Assignment Controller
    - Enrollment Controller
    - Student Controller

--- src/main/java/com/cst438/controller/EnrollmentController.java
+++ src/main/java/com/cst438/controller/EnrollmentController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.security.access.prepost.PreAuthorize;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -28,8 +30,10 @@ public class EnrollmentController {
     // user must be instructor for the section
 
     @GetMapping("/sections/{sectionNo}/enrollments")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_INSTRUCTOR')")
     public List<EnrollmentDTO> getEnrollments(
-            @PathVariable("sectionNo") int sectionNo ) {
+            @PathVariable("sectionNo") int sectionNo,
+            Principal principal) {
 
         Optional<Section> section = sectionRepository.findById(sectionNo);
         if (section.isEmpty()) {
@@ -66,7 +70,8 @@ public class EnrollmentController {
     // instructor uploads enrollments with the final grades for the section
     // user must be instructor for the section
     @PutMapping("/enrollments")
-    public void updateEnrollmentGrade(@RequestBody List<EnrollmentDTO> dlist) {
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_INSTRUCTOR')")
+    public void updateEnrollmentGrade(@RequestBody List<EnrollmentDTO> dlist, Principal principal) {
 
         // For each EnrollmentDTO in the list
         //  find the Enrollment entity using enrollmentId
